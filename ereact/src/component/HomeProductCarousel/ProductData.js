e0b7863b4dfedbb85a8f099b7d28e63f3a9852fb
laibaz16 <laibazahid16@gmail.com>
2023-12-09 17:24:37 -0500

    Implemented Products slider Home Page(overlay exists)
    Implemented a swipeable product slider for a home page. Hardcoded in this commit. The overlay needs to be fixed as well.

---
+++ ereact/src/component/HomeProductCarousel/ProductData.js
@@ -0,0 +1,79 @@
+export const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 1024 },
+      items: 4,
+      slidesToSlide: 2,
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 800 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 800, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+export const productData =[
+    {
+        id: 1,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/091d2018-484a-43d4-b85c-3b3d470c1e06/air-jordan-1-mid-se-big-kids-shoes-hfXjW4.png",
+        name: "Air Jordan 1 Mid SE",
+        price: "120",
+        Des:"Blue Tint/Summit White/White/Ice Blue",
+    },
+    {
+        id: 2,
+        image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/17c9cb39-4a80-4c27-8ff1-57028b5f91d6/air-force-1-high-womens-shoes-vBhHD4.png",
+        name: "Nike Air Force 1 High",
+        price: "62.97",
+        Des:"Wolf Grey/University Red/White/College Navy",
+ 
+    },
+
+    {
+        id: 3,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/874c4865-9bf9-477a-b99d-94257a8cced2/air-jordan-8-winterized-mens-shoes-HBQzn3.png",
+        name: "Air Jordan 8 Winterized",
+        price: "210",
+        Des:"Black/Metallic Silver/Gunsmoke",
+    },
+
+    {
+        id: 4,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/d2205c94-5951-4107-b4d1-18fd718e9989/air-jordan-1-high-og-big-kids-shoes-Wqf4f4.png",
+        name: "Air Jordan 1 High OG",
+        price: "140",
+        Des:"Tech Grey/Black/White/Muslin",
+    },
+
+    {
+        id: 5,
+        image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/4371ac31-97cd-4057-bd6e-41440e39f3b9/dunk-low-big-kids-shoes-Ss5wx2.png",
+        name: "Nike Dunk Low",
+        price: "90",
+        Des:"White/White/Black",
+    },
+
+    {
+        id: 6,
+        image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/0840b446-2393-40fd-b365-b7aa7d8ee635/dunk-low-big-kids-shoes-RZGMsR.png",
+        name: "Nike Dunk Low",
+        price: "90",
+        Des:"White/Light Iron Ore/Gum Light Brown/Smoke Grey",
+    },
+
+    {
+        id: 7,
+        image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/518aeb3b-b310-49d2-bc9e-76d390a01ef2/air-force-1-shadow-womens-shoes-kTgn9J.png",
+        name: "Nike Air Force 1 Shadow",
+        price: "101.97",
+        Des:"White/Fireberry/Light Lemon Twist/Blue Tint",
+    },
+
+];
--- ereact/src/contents/home/Home.js
+++ ereact/src/contents/home/Home.js
@@ -1,14 +1,31 @@
 import React from "react";
+import "../../component/HomeProductCarousel/ProductSlider.css";
 import Slider from "../../component/HomeSlider/Slider";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Product from "../../component/HomeProductCarousel/Product";
+import {productData, responsive } from "../../component/HomeProductCarousel/ProductData";
 
-const Home = () => {
-    // implement home page
+export default function Home() {
+    const product = productData.map((item) => (
+      <Product
+        name={item.name}
+        url={item.image}
+        price={item.price}
+        description={item.description}
+      />
+    ));
+  
     return (
-        <div>
-            Welcome to home page!
-            <Slider/>
+      <div className="HomeBody">
+        <div className="caresoul"><Slider></Slider>
+        <h1>React multi carousel</h1>
         </div>
+        <div className="Productcaresoul">
+            <Carousel showDots={true} responsive={responsive}>
+            {product}
+            </Carousel>
+        </div>
+      </div>
     );
-}
-
-export default Home;
+  }
